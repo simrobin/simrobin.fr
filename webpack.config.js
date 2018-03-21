33bdc2ee14fcee8b14a74fd2117574b5efbfa850
Simon Robin <simon.robin.info@gmail.com>
2018-03-21 23:07:12 +0100

    replace extract-text-plugin with mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,7 @@ const path = require('path');
 
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 
 const autoprefixer = require('autoprefixer');
@@ -14,20 +13,17 @@ const distPath = `${__dirname}/dist`;
 const ENV = process.env.npm_lifecycle_event;
 const isBuild = ENV.includes('build');
 
-const publicUrl = isBuild ? '' : 'http://localhost:8080'
+const publicUrl = isBuild ? '' : 'http://localhost:8080';
 const publicPath = isBuild ? '/' : 'http://localhost:8080/';
 
 const config = {};
 
-config.entry = [
-  `${srcPath}/js/main.js`,
-  `${srcPath}/css/style.scss`,
-];
+config.entry = [`${srcPath}/js/main.js`, `${srcPath}/css/style.scss`];
 
 config.output = {
   path: distPath,
   filename: 'js/main.[hash].js',
-  publicPath,
+  publicPath
 };
 
 config.module = {
@@ -35,64 +31,69 @@ config.module = {
     {
       test: /\.js$/,
       loader: 'babel-loader',
-      include: srcPath,
+      include: srcPath
     },
     {
       test: /\.scss$/,
-      loader: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [
-          { loader: 'css-loader', options: { minimize: isBuild } },
-          'postcss-loader',
-          'sass-loader',
-        ],
-      }),
-      include: srcPath,
+      loader: [
+        isBuild ? MiniCssExtractPlugin.loader : 'style-loader',
+        { loader: 'css-loader', options: { minimize: isBuild } },
+        'postcss-loader',
+        'sass-loader'
+      ],
+      include: srcPath
     },
     {
       test: /\.(eot|svg|ttf|woff|woff2)$/,
       loader: 'file-loader?name=fonts/[name].[ext]',
-      include: path.resolve(srcPath, 'fonts'),
+      include: path.resolve(srcPath, 'fonts')
     },
     {
       test: /\.(jpe?g|png|gif|svg)$/i,
       loader: 'file-loader?name=img/[name].[ext]',
-      include: path.resolve(srcPath, 'img'),
+      include: path.resolve(srcPath, 'img')
     },
     {
       test: /\.(pdf)$/i,
       loader: 'file-loader?name=files/[name].[ext]',
-      include: path.resolve(srcPath, 'files'),
+      include: path.resolve(srcPath, 'files')
     },
     {
       test: /favicon\.(ico)$/i,
       loader: 'file-loader?name=[name].[ext]',
-      include: srcPath,
-    },
-  ],
+      include: srcPath
+    }
+  ]
 };
 
 config.plugins = [
   new webpack.DefinePlugin({
     'process.env': JSON.stringify({
       PUBLIC_URL: publicUrl,
-      NODE_ENV: process.env.NODE_ENV,
-    }),
+      NODE_ENV: process.env.NODE_ENV
+    })
   }),
   new HtmlWebpackPlugin({
-    template: `${srcPath}/index.html`,
+    template: `${srcPath}/index.html`
   }),
-  new ExtractTextPlugin('css/style.[hash].css'),
   new SWPrecacheWebpackPlugin({
     dontCacheBustUrlsMatching: /\.\w{8}\./,
     filename: 'service-worker.js',
     minify: false,
     navigateFallback: publicUrl + '/index.html',
     navigateFallbackWhitelist: [/^(?!\/__).*/],
-    staticFileGlobsIgnorePatterns: [/\.map$/],
-  }),
+    staticFileGlobsIgnorePatterns: [/\.map$/]
+  })
 ];
 
-config.mode = isBuild ? 'production': 'development';
+if (isBuild) {
+  config.plugins.push(
+    new MiniCssExtractPlugin({
+      filename: 'css/style.[hash].css'
+    })
+  );
+}
+
+config.mode = isBuild ? 'production' : 'development';
 
 module.exports = config;
