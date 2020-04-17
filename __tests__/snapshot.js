ac92aa81cf7c595d979319f0892a38b603beb5c7
Simon Robin <simon.robin.info@gmail.com>
2020-04-17 17:16:22 +0200

    test(index):  add basic snapshot test for index page

---
+++ __tests__/snapshot.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Index from '../pages/index';
+
+test('Index should match snapshot', () => {
+  const { container } = render(<Index />);
+
+  expect(container).toMatchSnapshot();
+});
