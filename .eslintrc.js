45deaab6ad4236f4b476ad60eb958aab6eb57bac
Simon Robin <simon.robin.info@gmail.com>
2020-04-18 21:34:05 +0200

    chore(eslint): update eslint config and scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,9 @@
 module.exports = {
   root: true,
-  extends: [
-    'react-app',
-    'eslint:recommended',
-    'plugin:prettier/recommended'
-  ],
-  plugins: [
-    'prettier',
-  ],
+  extends: ['react-app', 'eslint:recommended', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
   },
 };
